f1a6d9a599b737aed0c018d50f818a482c4fb982
Esly Lescano <eslylescano@gmail.com>
2024-09-06 13:07:39 +0100

    Add database and docker

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = ''
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///default.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
